e5b309c60fa2bec2fe8de2c75fa8bc8c25d504f7
2894638479 <2894638479@qq.com>
2026-02-13 02:11:34 +0800

    revert modrinth publish slug

--- build.gradle.kts
+++ build.gradle.kts
@@ -102,8 +102,8 @@ publishMods {
     modrinth {
         accessToken = providers.environmentVariable("MODRINTH_TOKEN")
         projectId = "q8Q5ZFT7"
-        optional("ordsPcFz", "Ha28R6CL")
-        requires("FjVgWB2Y")
+        optional("fabric-language-kotlin","kotlin-for-forge")
+        requires("kotlinmcui-backend")
         minecraftVersionRange {
             start = "1.14"
             end = "latest"
